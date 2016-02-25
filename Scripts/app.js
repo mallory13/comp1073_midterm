16aba9250c1be80a3d97b436d4c4f81733c3b4f5
mallory13 <mallory.steele13@gmail.com>
2016-02-25 09:23:08 -0500

    set  up paragraph variables

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,9 +1,41 @@
+// Steele Portfolio
+// @author: Mallory Steele
+// Midterm
+
 // setup your IIFE (Immediately Invoked Function Expression)
 var travelReport = (function () {
 
 "use strict";
 console.log("App Started...")
 
+var filePath = location.pathname;
+    filePath = filePath.substring(filePath.lastIndexOf('/') + 1, filePath.lastIndexOf('.'));
+    console.log(filePath);
+    
+  var IntroParagraph;
+  var Folegandros;
+  var Alonissos;
+  var Spetses;
+  var Amorgos;
+  var Syros;
+  var Milos;
+  var Hydra;
+  var Ithaca;
+  var Gavdos;
+  
+
+                document.getElementById("IntroParagraph").innerHTML = "Skills:";
+                document.getElementById("Folegandros").innerHTML = "Skills:";
+                document.getElementById("Alonissos").innerHTML = "Skills:";
+                document.getElementById("Spetses").innerHTML = "Skills:";
+                document.getElementById("Amorgos").innerHTML = "Skills:";
+                document.getElementById("Syros").innerHTML = "Skills:";
+                document.getElementById("Milos").innerHTML = "Skills:";
+                document.getElementById("Hydra").innerHTML = "Skills:";
+                document.getElementById("Ithaca").innerHTML = "Skills:";
+                document.getElementById("Gavdos").innerHTML = "Skills:";
+                
+    
 
 
 
